fe61a08142c16b3f8298723d1277ecef84e30030
Serhii Stryzhak <strizhak.sergey@gmail.com>
2023-05-27 18:38:47 +0300

    removed unnecessary comments

--- script.js
+++ script.js
@@ -4,27 +4,4 @@ alert('Приємно познайомитись, ' + userName);
 
 userName = prompt('Введіть імʼя другого користувача');
 
-/// произошла ошибка
-
 alert('Привіт, ' + userName);
-
-
-// const header = document.body.querySelector('h1');
-
-// header.innerHTML = 'Привіт, це працює JavaScript';
-
-// window.onload = function() {
-//     let a = true;
-
-//     setInterval(() => {
-//         if (a) {
-//             document.body.style.color = 'red';
-//         } else {
-//             document.body.style.color = 'black';
-//         }
-//         a = !a;
-//     }, 500);
-//     document.body.querySelectorAll('.button')[1].addEventListener('click', () => {
-//         document.body.querySelector('h1').innerHTML = 'Привіт, це працює JavaScript';
-//     });
-// }
